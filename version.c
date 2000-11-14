75608947f3dd8e5bfa8b6c5ee5e1e4f3aa5f0668
Millis Miller <millis@faztek.org>
2000-11-14 14:28:06 +0000

    Display extra information on a version command, showing the following: Users registered, Maximium, games running, maximum

--- version.c
+++ version.c
@@ -5,10 +5,20 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "dip.h"
+#include "functions.h"
 
 void version(FILE * rfp)
 {
+	int ngames, mgames, muser;
+	ngames  = countgames();
+	muser = MAXUSER;
+	mgames = MAXGAMES;
+
 	fprintf(rfp, "Current version is njudge %s\n\n", JVERSION);
-}
+	fprintf(rfp, "%d users registered, maximum is %d\n", nded, muser);
+	fprintf(rfp, "%d games running, maximum is %d\n", ngames, mgames);
+	fprintf(rfp,"\n");
+} 
