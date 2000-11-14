8cce19eaab2c7302eb8677fbcf3c00c84ca6fcc7
Millis Miller <millis@faztek.org>
2000-11-14 14:28:19 +0000

    Extracted out judge-specific (i.e. site poliy) seetings to defaults.inc

--- defaults.h
+++ defaults.h
@@ -1,5 +1,8 @@
 /*
    ** $Log$
+   ** Revision 1.1  1998/02/28 17:51:14  david
+   ** Initial revision
+   **
    ** Revision 1.1  1996/10/20 12:29:45  rpaar
    ** Morrolan v9.0
    **
@@ -25,28 +28,6 @@
 #define _DEFAULTS_H
 
 #include "dip.h"
-
-#define D_ACCESS         A_DIFF	/* A_* */
-#define D_LEVEL          L_ANY	/* L_* */
-#define D_FLAGS          F_NORATE|F_NONMR|F_NOPLAGUE|F_GRACEDAYS	/* F_* */
-#define D_DEDICATE       -10
-#define D_MOVE_CLOCK     1410
-#define D_MOVE_MINT      12.0
-#define D_MOVE_NEXT      72.0
-#define D_MOVE_GRACE     168.0
-#define D_MOVE_DELAY     0.5
-#define D_MOVE_DAYS      "--TWTF-"
-#define D_RETREAT_CLOCK  -1
-#define D_RETREAT_MINT   0.0
-#define D_RETREAT_NEXT   24.0
-#define D_RETREAT_GRACE  72.0
-#define D_RETREAT_DELAY  0.5
-#define D_RETREAT_DAYS   "--TWTF-"
-#define D_BUILDS_CLOCK   -1
-#define D_BUILDS_MINT    0.0
-#define D_BUILDS_NEXT    24.0
-#define D_BUILDS_GRACE   72.0
-#define D_BUILDS_DELAY   0.5
-#define D_BUILDS_DAYS    "--TWTF-"
+#include "defaults.inc"
 
 #endif
