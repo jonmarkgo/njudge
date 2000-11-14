a8d9263459e2aca88d0d18ea3b3dbe4e4430a424
Millis Miller <millis@faztek.org>
2000-11-14 14:27:42 +0000

    Lots of changes, including  - get_die_magic() Get DIE_MAGIC value from .magic.dat (or cerate if not found)

--- lib.c
+++ lib.c
@@ -1,6 +1,9 @@
 
 /*
  * $Log$
+ * Revision 1.1  1998/02/28 17:49:42  david
+ * Initial revision
+ *
  * Revision 1.2  1996/11/05 23:09:52  rpaar
  * USIT changes to fix minor bugs
  */
@@ -37,11 +40,13 @@
 #include "dip.h"
 #include "functions.h"
 #include "porder.h"
-#include ".magic.h"
+/* .magic.h no longer used */
+/*#include ".magic.h"*/
+#include "mail.h"
 
 /* TODO make this static to the function, rather than global */
 char nowString[20];
-
+static long DIE_MAGIC;
 /****************************************************************************/
 
 void archive(char *file, char *subject)
@@ -100,7 +105,51 @@ int die(int n, int s)
 	return r;
 
 }
+/****** */
+long get_die_magic(void)
+{
+	/*
+	 * This function will see if a file called .magic.dat exists
+	 * (assumed previosuly created). If so, it will set DIE_MAGIC to
+	 * the value inside. If not, it will write a randon six-digit value
+	 * to this file 
+ 	 */
 
+    /* First, make sure value is generated only once */
+    static int first_pass = 0;
+    FILE *rand_ptr = NULL;
+    int n = 0;
+
+    if (!first_pass) { 
+
+    first_pass = 1;
+
+    rand_ptr = fopen("./.magic.dat", "r");
+    if (rand_ptr) {
+	n = fscanf(rand_ptr, "%ld", &DIE_MAGIC);
+	fclose(rand_ptr);
+    }
+
+    if (!rand_ptr || n != 1) { 
+    /* read errors, generate and write a random number */
+        rand_ptr = fopen("./.magic.dat", "w");
+	if (rand_ptr) {
+	    DIE_MAGIC = rand() % 1000000;
+	    n = fprintf(rand_ptr, "%6.6ld\n", DIE_MAGIC);
+	    fclose(rand_ptr);
+	}
+	if (!rand_ptr || n != 6) {
+		fprintf(log_fp, "Unable to write magic data to ./.magic.dat.\n");
+                bailout(1);
+	}
+    }
+  }
+  return DIE_MAGIC;
+}
+
+
+
+	
 /***********************************************************************/
 
 void die_rolls(int seed)
@@ -124,7 +173,7 @@ void die_rolls(int seed)
 	for (s = dipent.phase; *s; s++)
 		seed += *s;
 
-	srand(seed + DIE_MAGIC);
+	srand(seed + get_die_magic());
 
 }
 
@@ -143,7 +192,6 @@ int execute(char *command)
 	int i, status;
 	FILE *fp;
 	long now;
-
 
 	for (i = 0;; i++) {
 		time(&now);
@@ -428,6 +476,8 @@ char *autype(char c)
 		return ("a fleet");
 	else if (c == 'G')
 		return ("a garrison");
+	else if (c == 'W')
+		return ("a wing");
 	else if (c == 'T')
 		return ("an army/fleet");
 	else if (c == 'S')
@@ -472,6 +522,8 @@ char *utype(char c)
 		return ("fleet");
 	else if (c == 'G')
 		return ("garrison");
+	else if (c == 'W')
+                return ("wing");
 	else if (c == 'T')
 		return ("army/fleet");
 	else if (c == 'S')
@@ -491,6 +543,8 @@ char c;
 		return ("Fleet");
 	else if (c == 'G')
 		return ("Garrison");
+	else if (c == 'W')
+                return ("Wing");
 	else if (c == 'T')
 		return ("Army/Fleet");
 	else if (c == 'S')
@@ -499,6 +553,39 @@ char c;
 		return ("Unit");
 }
 
+/*
+ ** mov_type - return a string proper for unit type/province type
+ ** i.e. A Wing is 'over Spain' or 'over the Baltic'
+ ** A fleet in 'in Spain' or 'in the Baltic'
+ ** an army is 'in Spain' only
+ */
+
+char *mov_type( int prov_index, int unit_index)
+{
+	static char line[20];
+	char prov_type; 
+	char unit_type;
+
+	line[0] = '\0';
+	prov_type = pr[prov_index].type;
+	unit_type = unit[unit_index].type;
+
+	if (unit_type == 'W' )  {
+		strcpy(line, "over");
+		if (prov_type =='w' || prov_type == 'v' ) {
+			strcat(line, " the");
+		}
+	} else {
+		if (prov_type == 'w' || prov_type == 'v') {
+			strcpy(line, "in the");
+		} else {
+			strcpy(line, "in");
+		}
+	}
+	return line;
+}
+	
+	
 void wrap(FILE * fp, char *buf, int pos, int indent)
 {
 
@@ -564,4 +651,408 @@ char *NowString(void)
 
 	sprintf(nowString, "%12.12s: ", ctime(&now) + 4);
 	return (nowString);
+}
+
+int FindPower(int power_index)
+{
+	int i;
+	for (i = 0; i < dipent.n; i++)
+	{
+		if (dipent.players[i].power == power_index) return i;
+	}
+	return i;
+}
+
+
+int IsBlank(char *text)
+{
+        char *t = text;
+        int non_blank = 0;
+        while (*t != '\0' && !non_blank) {
+            if (0 == isspace(*t)) non_blank = 1;
+            t++;
+        }
+        return !non_blank;
+}
+
+int GetOneWord(char *text)
+{
+        char *t = text;
+        int non_blank = 0;
+        while (*t != '\0') {
+            if (non_blank == 1 && 0 != isspace(*t)) *t = '\0'; /*terminate at 1st non-blank*/
+            if (0 == isspace(*t)) non_blank = 1;
+            t++;
+        }
+        return non_blank;
+}
+
+int deadline_recursive( sequence *seq, int new, int *rec_count);
+
+#define DIPENT_NO_PRESS ((dipent.flags & F_NOWHITE) && !(dipent.flags & F_GREY))
+ 
+/****************************************************************************/
+/* Look and see if any requested absences */
+/* and adjust deadline parameter accordingly if so */
+
+int absence_adjust(long *deadline)
+{
+        int ret = 0; /* Set to 1 if adjusted times */
+        int i,j;
+
+        for (i =0; i < dipent.n; i++ )
+        {
+                for ( j=0; j < MAX_ABSENCES; j++)
+                {
+                        if (dipent.players[i].absence_start[j] < *deadline ) {
+                                if (dipent.players[i].absence_end[j] > *deadline ) {
+				    /* Check if a player has a pending move and (no press or strictwait)
+				       or game is quiet
+				       or is the master before adjusting the deadline */
+				    if ((dipent.players[i].status & SF_MOVE &&
+ 					(dipent.flags & F_STRWAIT || DIPENT_NO_PRESS)) || 
+				        (!(dipent.flags & F_STRWAIT) && !DIPENT_NO_PRESS) ||
+					dipent.flags & F_QUIET ||
+					dipent.players[i].power == MASTER)
+				    {
+                                        /* OK, we're in a requested deadline period */
+                                        /* so boot up the deadline accordingly      */
+                                        *deadline = dipent.players[i].absence_end[j];
+                                        ret = 1; /* Changed deadline */
+				    }
+                                }
+                        }
+                }
+        }
+        /* OK, now delete passed deadlines */
+        for (i =0; i < dipent.n; i++ )
+        {
+                for ( j=0; j < MAX_ABSENCES; j++)
+                {
+                    if (dipent.players[i].absence_start[j] != 0L ) {
+
+                        if (dipent.players[i].absence_start[j] <= *deadline ) {
+                                if (dipent.players[i].absence_end[j] <= *deadline ) {
+                                        /* OK, This deadline is no longer needed */
+					/* but bump up total anyway */
+					dipent.players[i].absence_total += 
+					    (dipent.players[i].absence_end[j] - 
+					    dipent.players[i].absence_end[j]);
+                                        dipent.players[i].absence_start[j] = 0L;
+                                        dipent.players[i].absence_end[j] = 0L;
+                                        dipent.players[i].absence_count--;
+                                }
+                        }
+                    }
+                }
+        }
+        return ret;
+}
+
+/*************************************************************************/
+
+int deadline(sequence *seq, int new)
+{
+        int rec_count = 0;
+        return deadline_recursive(seq, new, &rec_count);
+}
+
+
+/****************************************************************************/
+
+
+int deadline_recursive(sequence * seq, int new, int *rec_count)
+{
+
+/*
+ *  Compute a new deadline for this dip entry.
+ */
+
+        int i, k;
+        long now, temp;
+        struct tm *tm, *localtime();
+        int ret = 0;
+
+        (*rec_count)++;
+        if (*rec_count > 50) return 0; /* safety code */
+
+        time(&now);
+        if (dipent.phase[6] == 'X') {
+                dipent.process = now + 168 * HRS2SECS;
+                return 0;
+        }
+        if (!seq) {
+                if (!(seq = dipent.phase[5] == 'M' ? &dipent.movement :
+                      dipent.phase[5] == 'R' ? &dipent.retreat :
+                      dipent.phase[5] == 'B' ? &dipent.builds : NULL)) {
+                        fprintf(stderr, "Invalid phase [%s] in deadline for '%s'.\n",
+                                dipent.phase, dipent.name);
+                        fprintf(log_fp, "Invalid phase [%s] in deadline for '%s'.\n",
+                                dipent.phase, dipent.name);
+                        return E_FATAL;
+                }
+        }
+        /*
+           **  If the new flag is indicated, we assume a move has just completed
+           **  and we establish the next deadline.  We don't want to set the new
+           **  deadline earlier than the old one though.
+         */
+
+        if (new) {
+                temp = now + (int) (seq->next * HRS2SECS);
+
+                if (temp < dipent.deadline)
+                        temp = dipent.deadline;
+                /* OK, adjust the time requested according to pending absences */
+                ret = absence_adjust(&temp);
+
+                if (seq->clock >= 0) {
+                        tm = localtime(&temp);
+                        i = seq->clock * 60 - ((tm->tm_hour * 60 + tm->tm_min) * 60 + tm->tm_sec);
+                        if (i < 0)
+                                i += 24 * 60 * 60;
+                        temp += i;
+                }
+                for (k = 0; k < 8; k++) {
+                        tm = localtime(&temp);
+                        if (seq->days[tm->tm_wday] == '-')
+                                temp += 24 * 60 * 60;
+                        else if (islower(seq->days[tm->tm_wday]) && tm->tm_hour < 12)
+                                temp += (12 - tm->tm_hour) * 60 * 60 - tm->tm_min * 60 - tm->tm_sec;
+                        else
+                                break;
+                }
+
+                /* If old deadline was huge, keep it */
+		dipent.deadline = max(temp,  dipent.deadline) ;
+                dipent.process = temp;
+                temp += (int) (seq->grace * HRS2SECS);
+                if (dipent.flags & F_GRACEDAYS) {
+                        for (k = 0; k < 8; k++) {
+                                tm = localtime(&temp);
+                                if (seq->days[tm->tm_wday] == '-')
+                                        temp += 24 * 60 * 60;
+                                else if (islower(seq->days[tm->tm_wday]) && tm->tm_hour < 12)
+                                        temp += (12 - tm->tm_hour) * 60 * 60 - tm->tm_min * 60 - tm->tm_sec;
+                                else
+                                        break;
+                        }
+                }
+		/* If old grace was huge, keep it */
+                dipent.grace =  max(temp,  dipent.grace);
+                dipent.start = now + (int) (seq->mint * HRS2SECS);
+        }
+        /*
+           **  Figure out if we can bump up the process time.
+         */
+
+        temp = now + (int) (seq->delay * HRS2SECS);
+        for (i = 0; i < dipent.n; i++) {
+                if (dipent.players[i].power < 0)
+                        continue;
+
+                if (dipent.players[i].status & SF_PROCESS) {
+                        temp = now - 1 * HRS2SECS;
+                        dipent.players[i].status &= ~SF_PROCESS;
+                }
+                if (dipent.players[i].status & SF_WAIT && now < dipent.deadline) {
+                        temp = dipent.deadline;
+                        break;
+                }
+                if (WAITING(dipent.players[i].status)) {
+                        temp = now < dipent.deadline ? dipent.deadline :
+                            now > dipent.grace && dipent.flags & F_NONMR ? dipent.process :
+                            dipent.grace;
+                        break;
+                }
+        }
+
+        /*
+         *  We don't want to advance the process time beyond the deadline
+         *  here.  That will be done in 'process' when a reminder is sent
+         *  out to those who haven't gotten their orders in yet.
+         */
+
+        if ((dipent.process < dipent.deadline && temp < dipent.deadline) ||
+            temp < dipent.process)
+                dipent.process = max(dipent.start, temp);
+
+        if (ret == 1) {
+                /* We have adjusted the deadline due to an absence */
+                /* Recursively call to re-adjust if necessary */
+                deadline_recursive(seq, new, rec_count);
+                /* set the mailing flag so we can advise an absence adjust */
+                broadcast_absence_adjust = 1;
+        }
+        return 0;
+
+}
+
+
+/***** */
+/* This following function will remove all leading 
+ * spaces and convert double (or more) spaces to only one
+ */
+
+int despace(char *intext)
+{
+	char temp[350];
+	int space_count = 0;
+	char *s = intext;
+	char *outtext = temp;
+
+	/* First take away leading spaces */
+	while (isspace(*s)) s++;
+
+	while (*s) {
+	    if (isspace(*s)) {
+		space_count++;
+		if (space_count == 1) *outtext++ = ' ';
+	    }
+	    else {
+		*outtext++ = *s;
+		space_count = 0;
+	    }
+	    s++;
+	}
+	*outtext = '\0'; /* terminate the string */
+	strcpy(intext,temp);
+	return strlen(temp);
+}
+
+
+/***** */
+/* This function will display the absences for 
+ * a particular power (passsed as a parameter)
+ * Normal powers will see their absences only
+ * Master will see all absences
+ * Observer will see none
+ */
+
+void display_one_absences(int pindex, FILE *fptr)
+{
+	int i = pindex;
+	int k;
+
+        if (dipent.players[i].absence_count > 0) {
+           for (k = 0; k < MAX_ABSENCES;k++) {
+                if (dipent.players[i].absence_start[k] != 0) {
+                    fprintf(fptr, "%s: Absence requested from \n%s to ",
+                         powers[dipent.players[i].power], 
+			 ptime(&dipent.players[i].absence_start[k]));
+                    fprintf(fptr,"%s\n", ptime(&dipent.players[i].absence_end[k]));
+                }
+            }
+            fprintf(rfp,"\n");
+        }
+        return;
+}
+void display_absence(int pnum, FILE *fptr)
+{
+
+	int i;
+
+	switch (pnum) {
+
+	    case OBSERVER:
+	    case WILD_PLAYER:
+		/* Do nothing */
+		break;   
+
+	    case MASTER:
+		for (i = 0; i < MAXPLAYERS; i++ ) {
+		    display_one_absences(i, fptr);
+		}
+		break;
+
+	    default:
+		    display_one_absences(FindPower(pnum), fptr);
+	}
+	return;
+}
+
+/***********************************************************************/
+
+
+void AddOrderToOrder(char *text, int order)
+{
+    if (text == NULL) return;
+
+    switch (order)
+    {
+	case 'a':
+	    strcat(text,"AIRLIFT ");
+            break;
+	case 'c':
+	    strcat(text,"CONVOY ");
+	    break;
+	case 'm':
+	    strcat(text, "-> ");
+	    break;
+	case 't':
+	    strcat(text,"TRANSFORM ");
+	    break;
+	case 'p':
+	    strcat(text, "PROXY TO ");
+	    break;
+	case 'd':
+	    strcat(text, "DISBANDS ");
+	    break;
+	case 'h':
+	    strcat(text, "HOLD ");
+	    break;
+	case 's':
+	    strcat(text, "SUPPORT ");
+	    break;
+	case 'r':
+	    strcat(text, "REMOVE ");
+	    break;
+	case 'b':
+	    strcat(text,"BUILD ");
+	    break;
+	case 'w':
+	    strcat(text, "WAIVE ");
+	    break;
+	case 'x':
+	    break;
+	default:
+	   strcat(text,"*unknown order*");
+    }
+}
+
+void AddCoastToOrder(char *text, int c1)
+{
+    char temp[50];
+    if (text == NULL) return;
+    sprintf(temp, "(%s) ", mtype[c1]);
+    strcat(text, temp);
+}
+
+void AddUnitToOrder(char *text, int unit)
+{
+    if (text == NULL) return;
+    strcat(text, Utype(unit));
+    strcat(text," ");
+}
+
+void AddPlaceToOrder(char *text, int place)
+{
+    if (text == NULL) return;
+    if (place <= 0) return;
+    strcat(text, pr[place].name);
+    strcat(text," ");
+}
+void AddProvinceToOrder(char *text, int place)
+{
+	return AddPlaceToOrder(text,place);
+}
+void AddUnitProvinceToOrder(char *text, int unit, int place)
+{
+	AddUnitToOrder(text, unit);
+	AddPlaceToOrder(text,place);
+}
+void AddPowerToOrder(char *text, int power)
+{
+	if (text == NULL) return;
+	strcat(text,powers[power]);
 }
