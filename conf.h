b9a4602ebc857c43ace1ad0c0d685a54baff560e
Millis Miller <millis@faztek.org>
2000-11-14 14:28:18 +0000

    Added new dip.conf variable. Plus allowed dip.conf path to be passed in as a parameter

--- conf.h
+++ conf.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.1  1998/02/28 17:51:14  david
+ * Initial revision
+ *
  * Revision 1.2  1996/11/05 22:49:23  rpaar
  * NW's changes
  *
@@ -7,6 +10,13 @@
  * Morrolan v9.0
  */
 
+#define JUDGE_PATH      config("JUDGE_PATH")
+#define SMAIL_CMD	config("SMAIL_CMD")
+#define ATRUN_CMD	config("ATRUN_CMD")
+#define SUMMARY_CMD	config("SUMMARY_CMD")
+#define RUNLISTMAP_CMD	config("RUNLISTMAP_CMD")
+#define	RUNDIPMAP_CMD 	config("RUNDIPMAP_CMD")
+#define DIP_CMD		config("DIP_CMD")
 #define GAMES_MASTER    config("GAMES_MASTER")
 #define GAMES_OPENER	config("GAMES_OPENER")
 #define HALL_KEEPER     config("HALL_KEEPER")
@@ -28,12 +38,14 @@
 #define MIN_CONTROL     atoi(config("MIN_CONTROL"))
 #define LOCK_TIMEOUT    atoi(config("LOCK_TIMEOUT"))
 #define JUDGE_CODE      config("JUDGE_CODE")
+#define SPECIAL_PW	config("SPECIAL_PW")
 #define D_ONTIME        atoi(config("D_ONTIME"))
 #define D_LATE          atoi(config("D_LATE"))
 #define D_ABANDON       atoi(config("D_ABANDON"))
 #define D_CD            atoi(config("D_CD"))
 #define MAP_FILE        config("MAP_FILE")
 #define MASTER_FILE     config("MASTER_FILE")
+#define TMASTER_FILE	config("TMASTER_FILE")
 #define NO_CREATE       config("NO_CREATE")
 #define LOG_FILE        config("LOG_FILE")
 #define CUTOFF_LENGTH   atoi(config("CUTOFF_LENGTH"))
@@ -41,6 +53,10 @@
 #define DIE_EXPENSE     atoi(config("DIE_EXPENSE"))
 #define DIE_FAMPLAG     atoi(config("DIE_FAMPLAG"))
 #define DIE_INCOME      atoi(config("DIE_INCOME"))
+#define MAXGAMES	atoi(config("MAXGAMES"))
+#define SYSLOG_FLAG	atoi(config("SYSLOG_FLAG"))
+#define STATS_FLAG	atoi(config("STATS_FLAG"))
+#define STATS_DIR	config("STATS_DIR")
 
 /* this is the location of the judge configuration file,
  * it should probably be a full path name, as i don't think
@@ -48,12 +64,13 @@
  * that uses this may be called before the working directory is
  * set to a sane value.
  */
-
-#define CONFIG_FILE "/home/judge/dip/dip.conf"
+extern char * CONFIG_FILE;
+/* Now defined as a command line opton '-C' */
+extern char * CONFIG_DIR;
 
 void conf_usage(void);
 void conf_cmdline(int argc, char **argv);
-void conf_readfile(char *filename);
+void conf_readfile(char *directory, char *filename);
 void conf_print(FILE * where);
 int conf_init(void);
 
