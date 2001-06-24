728d83905e63d082ef985deb047bfcd219f28e8a
Tim Miller or Mario Becroft <judge@gem.win.co.nz>
2001-06-24 01:35:22 +0000

    Initial revision

---
+++ plyrdata.c
@@ -0,0 +1,273 @@
+/*
+ * plyrdata.c. Experimental file to log player statistics on the Judge
+ * for a new ratio based dedication system and other monitoring.
+ * Created first by Tim Miller, Feb 16, 2001. Preliminary structure
+ * completed on Mar 3, 2001.
+ *
+ * Coding by Tim Miller and Mario Becroft.
+ */
+/*
+$ID$
+*/
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include "plyrdata.h"
+
+int errno;
+
+PLYRDATA_HEADER header;
+
+static FILE *fp;
+
+FILE *create_plyrdata( void );
+
+int open_plyrdata( void ) {
+	/* Open the xPlyrData file. */
+	
+	if (fp)
+	{
+		fprintf(stderr, "Opening already open xplyrdata file.\n");
+	}
+	
+	fp = fopen(PLYRDATA_FILENAME, "r+");
+
+	if (!fp) {
+		if (errno == EACCES || errno == ENOENT) {
+			if (!(fp = create_plyrdata())) {
+				fprintf(stderr, "Error %d creating xplyrdata.\n", errno);
+				/*bailout(E_FATAL);*/
+				return 1;
+			}
+		}
+		else {
+			fprintf(stderr, "Error opening, xPlyrData file");
+			/*bailout(E_FATAL);*/
+			return 1;
+		}
+	}
+
+	if (fread( &header, sizeof( PLYRDATA_HEADER ), 1, fp ) < 1)  
+	{
+        	fprintf(stderr, "plyrdata header too short.\n");
+		/*bailout(E_FATAL);*/
+		return 1;
+	}
+	if (
+		header.magic != PLYRDATA_MAGIC ||
+		header.version > PLYRDATA_VERSION ||
+		header.record_size > 400 ||
+		header.header_size > 400 ) {
+		fprintf(stderr, "plyrdata header invalid.\n");
+		/*bailout(E_FATAL);*/
+		return 1;
+	}
+	return 0;
+}
+
+/********************************************************************/
+FILE *create_plyrdata( void ) {
+	FILE *f;
+	
+	f = fopen(PLYRDATA_FILENAME, "w+");
+	
+	if (f) {
+		memset(&header, 0, sizeof(PLYRDATA_HEADER));
+		header.magic = put_long(PLYRDATA_MAGIC);
+		header.version = put_long(PLYRDATA_VERSION);
+		header.header_size = put_long(sizeof(PLYRDATA_HEADER));
+		header.record_size = put_long(sizeof(PLYRDATA_RECORD));
+		if (fwrite(&header, sizeof(PLYRDATA_HEADER), 1, f) < 1)
+		{
+		 	fprintf(stderr, "could not write header to plyrdata.\n");
+			/*bailout(E_FATAL);*/
+			return NULL;
+		}
+		fseek( f, 0, SEEK_SET );
+	}
+	else
+	{
+	 	fprintf(stderr, "could not create plyrdata.\n");
+		/*bailout(E_FATAL);*/
+		return NULL;
+	}
+	
+	return f;
+}
+/***********************************************************************/
+long put_long( long in )
+{
+#ifdef LITTLE_ENDIAN
+	return
+	((in >> 24) & 0xFF) ||
+	((in >> 8)  & 0xFF00) ||
+	((in << 8)  & 0xFF0000) ||
+	((in << 24) & 0xFF000000);
+#else
+	return in;
+#endif
+}
+/******************************************************************/
+long get_long( long in )
+{
+#ifdef LITTLE_ENDIAN
+	return
+	((in >> 24) & 0xFF) ||
+	((in >> 8)  & 0xFF00) ||
+	((in << 8)  & 0xFF0000) ||
+	((in << 24) & 0xFF000000);
+#else
+	return in;
+#endif
+}
+
+/********************************************************************/
+int get_plyrdata_record( int recno, PLYRDATA_RECORD *record)
+{
+	size_t record_size;
+	fpos_t end;
+	fpos_t pos;
+	
+	/* Check that record is within the file */
+	fseek( fp, 0, SEEK_END );
+	end = ftell( fp );
+	pos = header.header_size + header.record_size * recno;
+	if (pos + header.record_size > end)
+		return 0;
+	if (fseek(fp, pos, SEEK_SET)) {
+		return 0;
+	}
+	record_size = sizeof(PLYRDATA_RECORD) < header.record_size ?
+		sizeof(PLYRDATA_RECORD) : header.record_size;
+	return fread(record, record_size, 1, fp);
+}
+/*******************************************************************/
+int put_plyrdata_record( int recno, PLYRDATA_RECORD *record)
+{
+	size_t record_size;
+	if (fseek(fp, header.header_size + header.record_size * recno,
+	SEEK_SET)) {
+		return 0;
+	}
+	record_size = sizeof(PLYRDATA_RECORD) < header.record_size ?
+		sizeof(PLYRDATA_RECORD) : header.record_size;
+	return fwrite(record, record_size, 1, fp);
+}
+/********************************************************************/
+uint32 get_data(int recno,PLYRDATA_TYPE type)
+{
+	/* This function will return data from a player's record */
+	PLYRDATA_RECORD rec;
+
+	/* First make sure we can read the record. */
+	if (get_plyrdata_record( recno, &rec ) < 1)
+                {
+                fprintf(stderr, "Could not read record #%d\n", recno );
+		/*
+		** If we can't read this, no data has been entered yet
+		** so we can just return 0 (since all fields will
+		** be 0 for new users.
+		*/
+		return 0;
+                }
+
+	/* Now see what data we need to retrieve and set it to output */
+  	switch (type) {
+
+    	case ontime:
+		return rec.ontime;
+		break;
+
+	case total:
+                return rec.total;
+                break;
+     
+	case started:
+		return rec.started;	
+		break;
+
+	case resigned:
+		return rec.resigned;
+		break;
+	
+	case tookover:
+		return rec.tookover;
+		break;
+
+     default:
+	fprintf(stderr, "Bad request for %d in XPlyrData file. Terminating.\n",
+  		(int)type);
+	return -1;
+    }
+   
+   return -1;
+}
+/********************************************************************/
+int put_data(int recno,PLYRDATA_TYPE type)
+{
+
+	/* First we have to find the appropriate record to put information
+	** into. If the record does not exist, we'll return a -1. 
+	** Mar 28, I've now changed this to create the record automatically
+	** if it does not exist.
+	*/
+	PLYRDATA_RECORD rec;
+
+	if (get_plyrdata_record( recno, &rec ) < 1)
+        {
+		/* OK, this record doesn't exist, better try to create it */
+		memset( &rec, 0, sizeof(PLYRDATA_RECORD));
+        }
+
+
+	/* See what sort of of info we want to put in to the record and
+	   act accordingly */
+	switch (type){
+  
+		case ontime:
+			rec.ontime++;
+			put_plyrdata_record( recno, &rec);
+			return 0;
+			break;
+
+		case total:
+			rec.total++;
+			put_plyrdata_record( recno, &rec);
+			return 0;
+			break;
+
+		case started:
+	                rec.started++;
+			put_plyrdata_record( recno, &rec);
+			return 0;
+			break;
+
+		case resigned:
+	                rec.resigned++;
+        	        put_plyrdata_record( recno, &rec );
+			return 0;
+			break;
+
+		case tookover:
+                       	rec.tookover++;
+                       	put_plyrdata_record( recno, &rec );
+                       	return 0;
+                        break;
+	
+		default:
+			fprintf(stderr, "Improper input for xplyrlog.");
+			return -1;
+	}
+}
+/******************************************************************/
+void close_plyrdata( void )
+{
+   if(fclose(fp))
+   {
+	fprintf(stderr, "Error closing XPlyrData file");
+	/*bailout(E_FATAL);*/
+   }
+}
+  
+
---
+++ recdump.c
@@ -0,0 +1,173 @@
+/*
+ * Recdump.c -- dumps player records into an output file -- also allows
+ * user to modify a particular record.
+ *
+ * Created 2001 - T. Miller and M. Becroft.
+ */
+
+/*
+$ID$
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "plyrdata.h"
+
+void writetodata(void);
+
+int main(int argc, char *argv[])
+{
+	int fd,i;
+	char *s;
+	FILE *fp;
+	PLYRDATA_RECORD rec;
+
+	if(argc == 1)
+	{
+		/* The user just wants a nice pretty text output. */
+		if((fd = open_plyrdata()) != 0)
+		{
+			printf("Error opening plyrdata file\n");
+			return 1;
+		} else {
+			if(!(fp = fopen("text.rec", "w")))
+			{
+				printf("Can't open text.rec file\n");
+				return 1;
+			}
+			for(i = 0;; i++)
+			{
+				if(get_plyrdata_record(i,&rec) == 0)
+				{
+					printf("Got %d records\n",i);
+					close_plyrdata();
+                        		fclose(fp);
+					return 0;
+				} else {
+					fprintf(fp,"Record %d\n",i);
+					fprintf(fp,"Ontime %lu\n",rec.ontime);
+					fprintf(fp,"Total %lu\n",rec.total);
+					fprintf(fp,"Started %lu\n",rec.started);
+					fprintf(fp,"Tookover %lu\n",rec.tookover);
+					fprintf(fp,"Resigned %lu\n",rec.resigned);
+				}
+			}
+		}
+	}
+	for(i = 1; i < argc; i++)
+	{
+		if(*argv[i] == '-')			
+		{
+			for(s = argv[1] + 1; *s; s++)
+			{
+				switch(*s)
+				{
+					case 'w':
+						/* User wants to write to
+						  the file. */
+						writetodata();
+						break;
+					default:
+						printf("Unrecognized flag '%c'\n",*s);
+						return 1;
+						break;
+				}
+			}
+		}
+	}
+	/* Nothing else to do, might as well return an error. */
+	return 1;
+	
+}
+void writetodata(void)
+{
+	PLYRDATA_RECORD rec;
+	int a,b,c,d;
+	if(open_plyrdata() != 0)
+	{
+		printf("Unable to open plyrdata file.\n");
+		exit(0);
+	}
+	printf("Which record do you want to modify?\n");
+	scanf("%d", &a);
+
+	/* Print the record for the user. */
+	if (get_plyrdata_record( a, &rec ) < 1)
+        {
+                printf( "Could not read record #%d\n", a);
+		if(a > 50000)
+		{
+			printf("%d is very large; really create this?\n",a);
+			printf("Type 0 for no, all else for yes.\n");
+			scanf("%d", &d);
+			if(d == 0) { 
+				exit(0);
+			}
+		}
+		printf("Creating record.\n");
+		memset(&rec,0,sizeof(PLYRDATA_RECORD));
+        }
+        else
+        {
+                printf( "Record #%d\n"
+                        "Ontime = %lu\n"
+                        "Total = %lu\n"
+                        "Started = %lu\n"
+                        "Resigned = %lu\n"
+                        "Tookover = %lu\n",
+                        a, rec.ontime, rec.total,
+                        rec.started, rec.resigned,
+                        rec.tookover );
+        }               
+	printf("Choose a field to modify, 1 for ontime, 2 for total, 3 for\n");
+	printf("started, 4 for resigned, or 5 for tookover.\n");
+	scanf("%d", &b);
+	printf("What should the new value for this field be?\n");
+	scanf("%d", &c);
+
+	switch(b)
+	{
+		case 1:
+			rec.ontime = c;
+			break;
+
+		case 2:
+			rec.total = c;
+			break;
+
+		case 3:
+			rec.started = c;
+			break;
+		
+		case 4:
+			rec.resigned = c;
+			break;
+
+		case 5:
+			rec.tookover = c;
+			break;
+
+		default:
+			printf("Invalid selection for field to modify.\n");
+			exit(0);
+	}
+	put_plyrdata_record(a, &rec);
+	if (get_plyrdata_record( a, &rec ) < 1)
+        {
+                printf( "Could not read record #%d\n", a);
+        }
+        else
+        {
+                printf( "Record #%d\n"
+                        "Ontime = %lu\n"
+                        "Total = %lu\n"
+                        "Started = %lu\n"
+                        "Resigned = %lu\n"
+                        "Tookover = %lu\n",
+                        a, rec.ontime, rec.total,
+                        rec.started, rec.resigned,
+                        rec.tookover );
+        }               
+	close_plyrdata();
+}
