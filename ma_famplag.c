5ae17b46ef77d5fd97981ee6b81c861b01f42692
Millis Miller <millis@faztek.org>
2001-07-01 23:19:36 +0000

    Added Storms

--- ma_famplag.c
+++ ma_famplag.c
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.1  1998/02/28 17:49:42  david
+ * Initial revision
+ *
  * Revision 1.1  1996/10/20 12:29:45  rpaar
  * Morrolan v9.0
  *
@@ -24,7 +27,25 @@ void fam_plag(int t)
 	int i, d, r, c, u, p;
 	char *s, buf[1024];
 	PFTAB(pftab);
+	char *typetext;
+	char Typetext[20];
 
+	switch (t)
+	{
+	    case FAMINE:
+		typetext = "famine";
+		break;
+	    case PLAGUE:
+		typetext = "plague";
+		break;
+	   case STORM:
+		typetext = "storm";
+		break;
+	   default:
+		bailout(1);  /* Unknown famine/plague type */
+	}
+	strcpy(Typetext,typetext);
+	Typetext[0] = toupper(typetext[0]);
 
 	/*
 	 *  Eliminate any units remaining in famine provinces at start of plague.
@@ -35,13 +56,14 @@ void fam_plag(int t)
 		for (u = 1; u <= nunit; u++) {
 			if (unit[u].owner <= 0)
 				continue;
-			if (is_infected(p = unit[u].loc)) {
+			if (is_infected(p = unit[u].loc) &&
+			   (t != STORM || unit[u].type == 'F')) {
 				if (!d++)
 					fputc('\n', rfp);
-				fprintf(rfp, "%s %s%s in %s eliminated by famine.\n",
+				fprintf(rfp, "%s %s%s in %s eliminated by %s.\n",
 					owners[unit[u].owner],
-				Stype(unit[u].stype), Utype(unit[u].type),
-					pr[p].name);
+				Stype(unit[u].stype), Utype(unit[u].type), 
+					pr[p].name, typetext);
 				unit[u].owner = 0;
 				remove_siege(unit[u].loc);
 			}
@@ -49,17 +71,44 @@ void fam_plag(int t)
 		for (p = 1; p <= npr; p++) {
 			remove_infection(p);
 		}
-		pftab = ptab;
-	} else {
-		pftab = ftab;
 	}
+	if (t==PLAGUE)
+		pftab = ptab;
+	else if (t== FAMINE) {
+		pftab = ftab;
+	} else
+	    pftab = stab;
 
 	if ((t == PLAGUE && !NO_PLAGUE) ||
-	    (t == FAMINE && !NO_FAMINE)) {
+	    (t == FAMINE && !NO_FAMINE) ||
+	    (t == STORM && !NO_STORMS)) {
 
 		die_rolls(DIE_FAMPLAG);
-		fprintf(rfp, "\n%s results: %d.   ", t == FAMINE ? "Famine" :
-			"Plague", d = die(2, 6));
+		if (dipent.xflags & XF_MACH2) {
+		    d = die(1,6);
+		    /* Now map to 2d6 settings */
+		   switch (d)
+		   {
+			case 1:  /* No diasters */
+			    d = 2;
+			    break;
+			case 2:
+			case 3: /* Row only */
+			    d = 4;
+			    break;
+			case 4:
+			case 5:
+			    d = 5;
+			    break;
+
+			default:
+			   d = 12;
+		    }
+		}
+		else
+		    d = die(2,6);
+
+		fprintf(rfp, "\n%s results: %d.   ", Typetext , d );
 		r = c = 0;
 		switch (d) {
 		case 2:
@@ -127,21 +176,22 @@ void fam_plag(int t)
 			wrap(rfp, buf, 18, 18);
 
 			/*
-			   **  Eliminate any unit in a plagued province.
+			   **  Eliminate any unit in a plagued/storm province.
 			 */
 
-			if (t == PLAGUE) {
+			if (t != FAMINE) {
 				d = 0;
 				for (u = 1; u <= nunit; u++) {
 					if (unit[u].owner <= 0)
 						continue;
-					if (is_infected(p = unit[u].loc)) {
+					if (is_infected(p = unit[u].loc) && 
+					   ( t != STORM || unit[u].type == 'F')) {
 						if (!d++)
 							fputc('\n', rfp);
-						fprintf(rfp, "%s %s%s in %s eliminated by plague.\n",
+						fprintf(rfp, "%s %s%s in %s eliminated by %s.\n",
 						   owners[unit[u].owner],
 							Stype(unit[u].stype), Utype(unit[u].type),
-							pr[p].name);
+							pr[p].name, typetext);
 						unit[u].owner = 0;
 						remove_siege(unit[u].loc);
 					}
