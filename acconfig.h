013ea83798303e386be4064666e615618fc0fbf3
Jaldahar Vyas <jaldhar@debian.org>
2003-09-09 19:21:00 +0000

    Whoops forgot to commit this.

---
+++ acconfig.h
@@ -0,0 +1,15 @@
+@BOTTOM@
+
+#ifdef HAVE_FLOCK
+  #include <sys/file.h>
+  #define lockfd(f,nb) flock(f, LOCK_EX | (nb ? LOCK_NB : 0))
+#else
+  #include <fcntl.h>
+  #define lockfd(f,nb)  lockf(f, (nb ? F_TLOCK : F_LOCK), 0)
+#endif
+
+#ifdef HAVE_MRAND48
+  #define random() mrand48()
+  #define srandom(seed) srand48((long)seed)
+#endif
+
