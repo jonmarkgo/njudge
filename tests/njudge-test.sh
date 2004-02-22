21dcc629250683473791ae020f37e3940469f379
Millis Miller <millis@faztek.org>
2004-02-22 23:33:47 +0000

    Test files, first version

--- tests/njudge-test.sh
+++ tests/njudge-test.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 # $Log$
+# Revision 1.3  2004/01/18 01:12:49  millis
+# Added credit to Jeff Smith
+#
 # Revision 1.2  2004/01/18 01:11:05  millis
 # Make diff work via Unix 'diff' command
 #
@@ -10,14 +13,15 @@
 
 use strict;
 use File::Copy;
+use Cwd;
 
 my $stat = 0;
-my $dip = "dip -A -a";
+my $dip = "dip -A -a ";
 my $outdir = "output";
 my $indir = "input";
 my $baselines = "baseline";
-#This should be changed to get absolute path where this script runs from ($0)
-my $heredir = "/home/judge/j-code/njudge/tests";
+my $heredir = cwd();
+my $dstat = 0;
 
 # Paramter $1 should be the directory to call
 # ./dip from, defaulting to /home/judge.
@@ -29,8 +33,16 @@ my $dir = $ARGV[0] ? $ARGV[0] : "/home/judge";
 
 #this stuff to make sure all data files are reset 
 chdir("..");
-system("rm $dir/dip.master");
-system("make install >& /dev/null");
+system("/bin/rm $dir/dip.master  $dir/dip.addr  $dir/dip.whois $dir/dip.ded");
+system("rm -rf $dir/D*");
+$stat = system("make install >& /dev/null");
+if ($stat)
+{
+     print("\nMake install returned error: check source code!\n\n");
+     exit(1);
+}
+my $master = "$dir/dip.master";
+
 chdir($heredir);
 
 #Create new smail to save emails too.
@@ -42,7 +54,8 @@ system ("cp -p $heredir/smail-test $dir/smail");
 chdir($indir);
 my @files = sort(<*.in>);
 chdir("..");
-my ($infile,$inpfile,$test,$text,$output,$baseline);
+my ($infile,$inpfile,$test,$text,$output,$baseline, $datetime, $masterbase, $masteroutput);
+
 foreach $infile (@files)
 {
 	$test = (split(/\./,$infile))[0];
@@ -50,12 +63,33 @@ foreach $infile (@files)
 	$output = "$outdir/$test.out";
 	$baseline = "$baselines/$test.out";
 	$inpfile = "$indir/$infile";
+	$datetime = "'1 Jan 2004'";
 
 
     # display the file $FILE.txt (i.e. cat $FILE.txt)
+   
+    unless (-e $text)
+    {
+        system("echo $text >> $text");
+    } 
+    
     print join("",slurp($text));
+#    <STDIN>;
+#Zap output mail file
+    unlink("$dir/smail.out");
 # call ./dip executable, passing the relevant $FILE.in file
-	system("$dir/$dip -C $dir -i $heredir/$inpfile >& $heredir/$output");
+        $dstat = system("$dir/$dip -C $dir -i $heredir/$inpfile -T $datetime >& $heredir/$output");
+
+	if ($dstat) 
+	{
+	    print("Dip returned error!");
+	    print("\n$dir/$dip -C $dir -i $heredir/$inpfile -T $datetime >& $heredir/$output\n");
+	    exit(1);
+	}
+
+#Add the description file to the end of the output (which should probably be empty if all worked ok)
+system("cat $text >> $heredir/$output");
+
 #append the email sent to outfile
 system ("cat $dir/smail.out >> $heredir/$output");
 
@@ -68,14 +102,36 @@ system ("cat $dir/smail.out >> $heredir/$output");
 		copy($output,$baseline);
 	}
 
-	$stat += system("diff $output $baseline");
+	if (system("diff $output $baseline")) 
+	{
+		$stat += 1;
+	}
+# Save dip.master file for further reference
+
+        $masterbase = "$baselines/$test.dip-master";
+	$masteroutput = "$outdir/$test.dip-master";
+	unless (-e $masterbase)
+	{
+		copy($master, $masterbase);
+	}
+	copy($master, $masteroutput);
+	if (system("diff $masteroutput $masterbase"))
+	{
+	     $stat += 1;
+	}
+
 #	cmpfiles($output,$baseline);
 }
 
+print ("Tests finished with $stat errors\n");
 #restore old smail
 system ("chmod +w $dir/smail");
 system ("cp -p $dir/smail.old $dir/smail");
-exit($stat);
+if ($stat) 
+{
+    exit(1);
+}
+exit(0);
 
 sub cmpfiles
 {
