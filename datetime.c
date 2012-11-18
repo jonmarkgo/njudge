9459b735e57d050cb6800b4eb017f2b263ce54f3
Conny Wagenius <cjw@tuxxor.net>
2012-11-18 07:19:00 +0000

    Added datetime.c and datetime.h with function datetime_epoc_from_string
    git-svn-id: svn://voidptr.se/njudge-cjw/trunk@81 ecda452d-b1b8-4988-b739-6bd04b8c14cc

---
+++ datetime.c
@@ -0,0 +1,46 @@
+/*
+ * datetime.c
+ *
+ *  Created on: Nov 18, 2012
+ *      Author: cjw
+ */
+
+#include <glib.h>
+#include <time.h>
+#include <stdio.h>
+
+static const char* abmon[12] = {
+		"jan", "feb", "mar", "apr", "may", "jun",
+		"jul", "aug", "sep", "oct", "nov", "dec"
+};
+
+time_t datetime_epoc_from_string(const char* str) {
+
+	guint     epoc = 0;
+	guint     idx;
+	guint     parsed;
+	gchar*    str_month[4];
+	gchar*    cptr = abmon;
+	struct tm date = {0};
+
+	/* copy and strip date string */
+	dt_str = g_strstrip(g_strdup(str));
+
+	parsed = sscanf(str, "%d %3s %d", date.tm_mday, str_month, date.tm_year);
+	if (parsed < 3) goto exit_str_to_epochdate;
+
+	for (idx = 0; idx < 12; idx ++) {
+		if (!strcasecmp(abmon[idx], str_month)) {
+			date.tm_mon = idx;
+		}
+	}
+
+	if (idx == 12) goto exit_str_to_epocdate;
+
+	epoc = mktime(&date);
+
+exit_str_to_epocdate:
+
+	return epoc;
+
+}
---
+++ datetime.h
@@ -0,0 +1,13 @@
+/*
+ * datetime.h
+ *
+ *  Created on: Nov 18, 2012
+ *      Author: cjw
+ */
+
+#ifndef DATETIME_H_
+#define DATETIME_H_
+
+time_t datetime_epoc_from_string(const char* str);
+
+#endif /* DATETIME_H_ */
