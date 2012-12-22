d498d31f63442c671c2d746115f58c9dfd6d9bf8
Conny Wagenius <cjw@tuxxor.net>
2012-12-22 09:50:22 +0000

    Added unlock macros to acconfig.h
    git-svn-id: svn://voidptr.se/njudge-cjw/trunk@95 ecda452d-b1b8-4988-b739-6bd04b8c14cc

--- acconfig.h
+++ acconfig.h
@@ -3,9 +3,11 @@
 #ifdef HAVE_FLOCK
   #include <sys/file.h>
   #define lockfd(f,nb) flock(f, LOCK_EX | (nb ? LOCK_NB : 0))
+  #define unlockfd(f, nb) flock(f, 0)
 #else
   #include <unistd.h>
   #define lockfd(f,nb)  lockf(f, (nb ? F_TLOCK : F_LOCK), 0)
+  #define unlockfd(f, nb) lockf(f, 0, 0)
 #endif
 
 #ifdef HAVE_MRAND48
