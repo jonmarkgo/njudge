b333de55d43f934a7537d9eb6da624a73d0f58a1
Conny Wagenius <cjw@tuxxor.net>
2013-06-27 06:34:19 +0000

    Change error message for invalid mail address
    git-svn-id: svn://voidptr.se/njudge-cjw/trunk@133 ecda452d-b1b8-4988-b739-6bd04b8c14cc

--- user.c
+++ user.c
@@ -30,7 +30,7 @@ int user_add_mail(user_t* usr, char* mail, GError** gerr) {
 	}
 	if (!g_regex_match_simple("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$",
 			mail, G_REGEX_CASELESS, 0)) {
-		g_set_error(gerr, USER_MODULE, MAIL_INVALID, "mail is invalid");
+		g_set_error(gerr, USER_MODULE, MAIL_INVALID, "mail address is invalid");
 		goto exit_user_add_mail;
 	}
 
