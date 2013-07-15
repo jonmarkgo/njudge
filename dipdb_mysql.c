77c73526db0be5750ba41448c50ea48898ab7626
Conny Wagenius <cjw@tuxxor.net>
2013-07-15 07:43:48 +0000

    Created dipdb_mysql.c and .h
    git-svn-id: svn://voidptr.se/njudge-cjw/trunk@156 ecda452d-b1b8-4988-b739-6bd04b8c14cc

---
+++ dipdb_mysql.c
@@ -0,0 +1,17 @@
+/*
+ * dipdb_mysql.c
+ *
+ *  Created on: Jul 15, 2013
+ *      Author: cjw
+ */
+
+#include <glib.h>
+#include <stdio.h>
+
+gint dipdb_mysql_update_value(gchar* table, gchar* column, gpointer* value) {
+
+	int RESULT = 0;
+
+	return RESULT;
+
+}
---
+++ dipdb_mysql.h
@@ -0,0 +1,12 @@
+/*
+ * dipdb_mysql.h
+ *
+ *  Created on: Jul 15, 2013
+ *      Author: cjw
+ */
+
+#ifndef DIPDB_MYSQL_H_
+#define DIPDB_MYSQL_H_
+
+
+#endif /* DIPDB_MYSQL_H_ */
