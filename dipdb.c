218af9e918ce9a3f426f845b387f23a54554ae5d
Conny Wagenius <cjw@tuxxor.net>
2013-07-15 18:28:47 +0000

    Added functionality for init db interface
    git-svn-id: svn://voidptr.se/njudge-cjw/trunk@160 ecda452d-b1b8-4988-b739-6bd04b8c14cc

--- dipdb.c
+++ dipdb.c
@@ -7,11 +7,44 @@
 
 #include <glib.h>
 #include <stdio.h>
+#include <string.h>
 
-gboolean dipdb_init(GError** gerr) {
+#include "dipdb.h"
+#include "dipdb_mysql.h"
+
+static void setup_mysql(void);
+
+gboolean (*dipdb_init)(GError** gerr);
+
+static GQuark dipdb_error_quark(void) {
+
+	return g_quark_from_static_string("DIPDB");
+
+}
+gboolean dipdb_interface_init(GError** gerr) {
 
 	gboolean result = FALSE;
 
+	if (!strcasecmp(conf_get("db_interface"), "mysql")) setup_mysql();
+	else if (!strcasecmp(conf_get("db_interface"), "newdip")) {
+		g_set_error(gerr, DIPDB, DIPDB_IF_NOT_IMPLEMENTED,
+				"database interface not yet implemented");
+		goto exit_dipdb_interface_init;
+	} else {
+		g_set_error(gerr, DIPDB, DIPDB_UNKNOWN_IF,
+				"unknown database interface: %s", conf_get("db_interface"));
+		goto exit_dipdb_interface_init;
+	}
+
+	result = TRUE;
+
+exit_dipdb_interface_init:
+
 	return result;
+
+}
+static void setup_mysql(void) {
+
+	dipdb_init = dipdb_mysql_init;
 
 }
--- dipdb.h
+++ dipdb.h
@@ -8,5 +8,15 @@
 #ifndef DIPDB_H_
 #define DIPDB_H_
 
+#define DIPDB		dipdb_error_quark()
+
+typedef enum dipdb_error_e {
+
+	DIPDB_IF_NOT_IMPLEMENTED = 1,
+	DIPDB_UNKNOWN_IF
+
+} DipDBError;
+
+gboolean dipdb_interface_init(GError** gerr);
 
 #endif /* DIPDB_H_ */
