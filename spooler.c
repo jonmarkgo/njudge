5b26923a8a03091c5811e0069153c127d625eec9
Conny Wagenius <cjw@tuxxor.net>
2013-07-17 14:26:13 +0000

    Added spooler.c and spooler.h
    git-svn-id: svn://voidptr.se/njudge-cjw/trunk@162 ecda452d-b1b8-4988-b739-6bd04b8c14cc

---
+++ spooler.c
@@ -0,0 +1,7 @@
+/*
+ * spooler.c
+ *
+ *  Created on: Jul 17, 2013
+ *      Author: cjw
+ */
+
---
+++ spooler.h
@@ -0,0 +1,12 @@
+/*
+ * spooler.h
+ *
+ *  Created on: Jul 17, 2013
+ *      Author: cjw
+ */
+
+#ifndef SPOOLER_H_
+#define SPOOLER_H_
+
+
+#endif /* SPOOLER_H_ */
